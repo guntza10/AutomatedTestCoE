f83ba36baf47da49793a37f50833c52040f996ba
DESKTOP-HH24VDA\Diamond <woraput@kkumail.com>
2022-04-21 17:26:15 +0700

    edit delay flow

--- cypress/integration/CoE/degree_certificated_tabee.js
+++ cypress/integration/CoE/degree_certificated_tabee.js
@@ -271,11 +271,13 @@ describe("Test Degree Certificated", () => {
       .then((div) => {
         cy.wrap(div).get('div[class="item"').click();
       });
+    cy.wait(1000);
     cy.get('div[class="cms-tabee-textbox-ourline"]')
       .eq(0)
       .then((div) => {
         cy.wrap(div).get("input").eq(1).type("มหาวิทยาลัยปทุม");
       });
+    cy.wait(1000);
     cy.get('div[class="cms-tabee-textbox-ourline"]')
       .eq(1)
       .then((div) => {
@@ -286,6 +288,7 @@ describe("Test Degree Certificated", () => {
     cy.get('div[class="btn coe-red-white"]').eq(0).click();
     cy.wait(3000);
     cy.get('div[class="button-items-right clickable"]').click();
+    cy.wait(3000);
     cy.get('input[placeholder="Card Number"]').type("4111 1111 1111 1111");
     cy.get('input[placeholder="Full Name"]').type("abc");
     cy.get('input[type="number"]').eq(0).type("08");
